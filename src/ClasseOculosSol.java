dfa7d24a0b73b114b1615646cc757b574a58fafb
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-11-28 10:41:32 -0300

    Adicionados construtores na classe óculosSol

--- src/ClasseOculosSol.java
+++ src/ClasseOculosSol.java
@@ -1,4 +1,20 @@
 
-public class ClasseOculosSol {
+public class ClasseOculosSol extends ClasseProdutos{
 
+	String corLente;
+	
+	public ClasseOculosSol(String cor, float preco, String pigmentacao){
+		super(cor, preco);
+		
+		this.corLente = pigmentacao;
+	}
+	
+	public void setColorLente(String pigmento){
+		this.corLente = pigmento;
+	}
+	
+	public String getCorLente(){
+		return corLente;
+	}
+	
 }
