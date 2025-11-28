99995854e19cfea77edfa2c291c34b6e31f1b367
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-11-28 10:25:46 -0300

    Criados os atributos, construtores e getters e setters na classe produtos

--- src/ClasseProdutos.java
+++ src/ClasseProdutos.java
@@ -1,4 +1,24 @@
 
 public class ClasseProdutos {
-
+	
+	String cor;
+	float preco;
+	
+	public ClasseProdutos(String c, float p){
+		this.cor = c;
+		this.preco = p;
+	}
+	
+	public float getPreco(){
+		return preco;
+	}
+	public String getCor(){
+		return cor;
+	}
+	public void setCor(String color){
+		this.cor = color;
+	}
+	public void setPreco(float price){
+		this.preco = price;
+	}
 }
